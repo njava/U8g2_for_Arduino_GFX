77c257019b0bbffd10c7154b8dc16f470d4687fb
wqk@hackmac <wqk@hackmac.com>
2021-04-05 10:32:04 +0800

    fix Boxed

--- examples/Boxed_Text_ILI9225/Boxed_Text_ILI9225.ino
+++ examples/Boxed_Text_ILI9225/Boxed_Text_ILI9225.ino
@@ -1,21 +1,23 @@
 /*
 
-  Hello_Adafruit_SSD1306.ino
+  Boxed_Text_ILI9225.ino
 
   Demonstrates how to draw a fitting box around a text.
 
   List of all U8g2 fonts: https://github.com/olikraus/u8g2/wiki/fntlistall
       
 */
-#include <Adafruit_SSD1306.h>
-#include <U8g2_for_Adafruit_GFX.h>
+#include <Arduino_GFX_Library.h>
+#include <U8g2_for_Arduino_GFX.h>
 
-Adafruit_SSD1306 display(/*MOSI*/ 11, /*CLK*/ 13, /*DC*/ 9, /*RESET*/ 8, /*CS*/ 10);
-U8G2_FOR_ADAFRUIT_GFX u8g2_for_adafruit_gfx;
+// ILI9225 LCD 176x220
+Arduino_DataBus *bus = new Arduino_ESP32PAR8(5 /*TFT_DC*/, 25 /*TFT_CS*/, 26 /* WR */, 13 /* RD */, 19, 21, 22, 23, 18, 12, 14, 15);
+Arduino_ILI9225 *gfx = new Arduino_ILI9225(bus, 27/*TFT_RST*/, 2);
+U8G2_FOR_ARDUINO_GFX u8g2_for_arduino_gfx;
 
 void setup() {
-  display.begin(SSD1306_SWITCHCAPVCC);
-  u8g2_for_adafruit_gfx.begin(display);                 // connect u8g2 procedures to Adafruit GFX
+    gfx->begin();
+	u8g2_for_arduino_gfx.begin(*gfx);                 // connect u8g2 procedures to Adafruit GFX
 }
 
 void loop() {  
@@ -26,18 +28,18 @@ void loop() {
   /* desired position of the text */
   int16_t x = 4;
   int16_t y = 25;
-  display.clearDisplay();                               // clear the graphcis buffer  
-  u8g2_for_adafruit_gfx.setFont(u8g2_font_helvB14_tf);  // select u8g2 font from here: https://github.com/olikraus/u8g2/wiki/fntlistall
-  u8g2_for_adafruit_gfx.setFontMode(1);                 // use u8g2 transparent mode (this is default)
-  u8g2_for_adafruit_gfx.setFontDirection(0);            // left to right (this is default)
-  u8g2_for_adafruit_gfx.setForegroundColor(WHITE);      // apply Adafruit GFX color
-  u8g2_for_adafruit_gfx.setCursor(x, y);                // start writing at this position
-  u8g2_for_adafruit_gfx.print(s);
+  gfx->fillScreen(BLACK);                               // clear the graphcis buffer  
+  u8g2_for_arduino_gfx.setFont(u8g2_font_helvB14_tf);  // select u8g2 font from here: https://github.com/olikraus/u8g2/wiki/fntlistall
+  u8g2_for_arduino_gfx.setFontMode(1);                 // use u8g2 transparent mode (this is default)
+  u8g2_for_arduino_gfx.setFontDirection(0);            // left to right (this is default)
+  u8g2_for_arduino_gfx.setForegroundColor(WHITE);      // apply Adafruit GFX color
+  u8g2_for_arduino_gfx.setCursor(x, y);                // start writing at this position
+  u8g2_for_arduino_gfx.print(s);
   /* calculate the size of the box into which the text will fit */
   height = u8g2_for_adafruit_gfx.getFontAscent() - u8g2_for_adafruit_gfx.getFontDescent();
   width = u8g2_for_adafruit_gfx.getUTF8Width(s);
   /* draw the box around the text*/
-  display.drawRect(x, y-u8g2_for_adafruit_gfx.getFontAscent(), width, height, WHITE);
-  display.display();                                    // make everything visible
+  gfx->drawRect(x, y-u8g2_for_adafruit_gfx.getFontAscent(), width, height, WHITE);
+                                     // make everything visible
   delay(2000);
 } 
